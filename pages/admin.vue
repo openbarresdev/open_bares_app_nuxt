5afaf5716bff46415bbb98d97479ecb4f8f8e258
Donald Magoua <donald.m@glotelho.com>
2025-10-14 01:15:34 +0200

    Update: Fixed menu on scroll

--- pages/admin.vue
+++ pages/admin.vue
@@ -10,11 +10,11 @@
             <!-- ---------- END MAIN SIDEBAR ---------- -->
             <div class="z-1 flex flex-col justify-between lg:space-y-6 space-y-4 lg:p-6 p-3 lg:ms-81">
                 <!-- ---------- HEADER ---------- -->
-                <CommonNavbar @toggle-aside="activateAside"/>
+                <CommonNavbar @toggle-aside="activateAside" class="max-lg:fixed max-lg:top-0 max-lg:w-full max-lg:z-50 max-lg:-mx-3 max-lg:px-0"/>
                 <!-- ---------- END HEADER ---------- -->
 
                 <!-- ---------- MAIN CONTENT ---------- -->
-                <main class="flex-1">
+                <main class="flex-1 max-lg:mt-16">
                     <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
                         <div class="card min-h-200 w-full md:col-span-2">
                             <div class="card-body max-lg:p-2 py-2">
