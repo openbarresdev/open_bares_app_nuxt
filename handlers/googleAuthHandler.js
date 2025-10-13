abb6498e88312d0702f83a29b8679878188114b6
Donald Magoua <donald.m@glotelho.com>
2025-10-13 21:31:04 +0200

    Update env path to oogle Auth

--- handlers/googleAuthHandler.js
+++ handlers/googleAuthHandler.js
@@ -2,8 +2,10 @@ export const googleAuthHandler = async () => {
   const config = useRuntimeConfig();
     
   const params = new URLSearchParams({
-    client_id: config.public.GOOGLE_CLIENT_ID,
-    redirect_uri: `${config.public.AUTH_ORIGIN}/api/auth/googleAuth`,
+    client_id: config.public.GOOGLE_CLIENT_ID || process.env.GOOGLE_CLIENT_ID,
+    redirect_uri:
+      `${config.public.AUTH_ORIGIN}/api/auth/googleAuth` ||
+      `${process.env.AUTH_ORIGIN}/api/auth/googleAuth`,
     response_type: "code",
     scope: "openid email profile",
   });
