8774a5a1324ea4c5a5e875f353459e6ed4af9494
Donald Magoua <donald.m@glotelho.com>
2026-02-06 17:08:32 +0100

    Fixed: Admin in fetch list

--- server/api/admin/users.get.ts
+++ server/api/admin/users.get.ts
@@ -3,13 +3,15 @@ import { prisma } from "~/server/lib/prisma";
 export default defineEventHandler(async (event) => {
 
   const users = await prisma.user.findMany({
+    where: {
+      role: "USER",
+    },
     select: {
       id: true,
       email: true,
       role: true,
       isActive: true,
       createdAt: true,
-
       project: {
         select: {
           id: true,
